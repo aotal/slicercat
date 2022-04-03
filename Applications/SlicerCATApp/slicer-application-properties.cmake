cf0c62ec36b2cc2de8ef6dc0dc380bac1165cfeb
aotal <aotalpalacin@gmail.com>
2022-04-03 18:57:17 +0200

    Initial commit

---
+++ Applications/SlicerCATApp/slicer-application-properties.cmake
@@ -0,0 +1,35 @@
+
+set(APPLICATION_NAME
+  SlicerCAT
+  )
+
+set(VERSION_MAJOR
+  1
+  )
+set(VERSION_MINOR
+  1
+  )
+set(VERSION_PATCH
+  1
+  )
+
+set(DESCRIPTION_SUMMARY
+  "Customized version of Slicer"
+  )
+set(DESCRIPTION_FILE
+  ${Slicer_SOURCE_DIR}/README.txt
+  )
+
+set(LAUNCHER_SPLASHSCREEN_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Images/SplashScreen.png"
+  )
+set(APPLE_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.icns"
+  )
+set(WIN_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.ico"
+  )
+
+set(LICENSE_FILE
+  "${SlicerCAT_SOURCE_DIR}/LICENSE"
+  )
---
+++ README.md
@@ -0,0 +1,26 @@
+SlicerCAT by Kitware, Inc.
+================================
+
+Customized version of Slicer
+
+_This project is in active development and may change from version to version without notice,_
+
+## Features
+
+_List key features_
+
+
+## Table of content
+
+* [Features](#features)
+* [Maintainers](#maintainers)
+
+
+## Maintainers
+
+* [Contributing](CONTRIBUTING.md)
+* [Building](BUILD.md)
+
+
+![SlicerCAT by Kitware, Inc.](Applications/SlicerCATApp/Resources/Images/LogoFull.png?raw=true)
+
